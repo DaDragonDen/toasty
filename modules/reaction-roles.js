13dd02b3a9a530c6e7ceb16ae481a40fb3bd469c
Christian Toney <37517470+Sudobeast@users.noreply.github.com>
2021-08-01 00:40:05 -0400

    Modularize reaction roles

--- modules/reaction-roles.js
+++ modules/reaction-roles.js
@@ -1,9 +1,11 @@
-module.exports = async (collection, botMember, userMember, msg, emoji) => {
+module.exports = async (collection, botMember, userMember, msg, emoji, add) => {
 
-  // Make sure they aren't a bot
-  if (userMember.bot) return;
-    
-  // Check if the message is a role message
+  try {
+
+    // Make sure they aren't a bot
+    if (userMember.bot) return;
+      
+    // Check if the message is a role message
   const roleMessageInfo = await collection.findOne({roleMessageId: msg.id, emoji: emoji.id || emoji.name});
   if (roleMessageInfo) {
 
@@ -13,11 +15,25 @@ module.exports = async (collection, botMember, userMember, msg, emoji) => {
       console.log("\x1b[33m%s\x1b[0m", "[Reaction Roles]: I don't have permission to add roles in Guild " + msg.channel.guild.id);
       return;
 
-    }
+      }
+        
+      // Add or remove the role
+      if (add) {
+
+        userMember.addRole(roleMessageInfo.roleId, "Reacted to message associated with the role");
+
+      } else {
+
+        userMember.removeRole(roleMessageInfo.roleId, "Removed reaction of message associated with the role");
+
+      }
       
-    // Add the role
-    await userMember.addRole(roleMessageInfo.roleId, "Reacted to message associated with the role");
-    
+    }
+
+  } catch (err) {
+
+    console.log("\x1b[33m%s\x1b[0m", "[Reaction Roles]: Something bad happened when handling reaction roles: " + err);
+
   }
 
 };
