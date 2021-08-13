eddb87888a80289f41a68f8db8e4de3f29d8ecb4
Christian Toney <37517470+Sudobeast@users.noreply.github.com>
2021-08-13 08:32:02 -0400

    Don't tell DDD when another guild's members leave

--- app.js
+++ app.js
@@ -190,7 +190,7 @@ bot.once("ready", async () => {
   bot.on("guildMemberRemove", async (guild, member) => {
 
     // Tell the admins
-    await bot.createMessage("497607965080027138", "<@" + member.id + "> left the server.");
+    if (guild.id === "497607965080027136") await bot.createMessage("497607965080027138", "<@" + member.id + "> left the server.");
 
   });
 
