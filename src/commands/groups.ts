a8c244be2f7c3d6ff29f99fbd52acc4de9d54e30
Christian Toney <37517470+Sudobeast@users.noreply.github.com>
2022-08-19 01:06:37 -0400

    add checks

--- src/commands/groups.ts
+++ src/commands/groups.ts
@@ -1,5 +1,14 @@
-import { Role } from "eris";
+import { InteractionDataOptionsSubCommand, Role, SelectMenuOptions } from "eris";
 import { Command } from "../commands.js";
+
+interface MenuDataProperties {
+  [guildId: string]: {
+    page: number;
+    selectedRoles: Role[];
+  }
+}
+
+const menuData: MenuDataProperties = {};
 
 new Command({
   name: "groups",
@@ -53,6 +62,23 @@ new Command({
 
     }
 
+    // Verify that we have a base role.
+    const subCommand = interaction.data.options?.find((option) => option.type === 1) as InteractionDataOptionsSubCommand;
+    if (!subCommand) {
+
+      await interaction.createFollowup("Couldn't find sub-command.");
+      return;
+
+    }
+
+    const baseRole = subCommand.options?.find((option) => option.name === "base")?.value;
+    if (!baseRole) {
+      
+      await interaction.createFollowup("You didn't give me a role to work with!");
+      return;
+
+    }
+
     // Make sure they have permission to manage roles.
     if (!member.permissions.has("manageRoles")) {
 
@@ -61,13 +87,88 @@ new Command({
 
     }
 
-    // Present the list of server roles to them.
-    const selectedRoles: Role[] = [];
-    while (!selectedRoles[0]) {
+    // Make sure there is at least one role in the guild.
+    let roles = guild.roles.filter(() => true);
+    if (roles.length === 0) {
+
+      await interaction.createFollowup("There aren't any roles in this server!");
+      return;
 
     }
-    const {roles} = guild;
 
+    // Present the roles to the user.
+    let selectMenuOptions: SelectMenuOptions[] = [];
+    for (let i = 0; roles.length >= i; i++) {
+
+      const {name, id} = roles[i];
+      selectMenuOptions[i] = {
+        label: name,
+        value: id
+      };
+
+      if (selectMenuOptions.length === 25) break;
+
+    }
+
+    await interaction.createFollowup({
+      content: `What roles do you want to associate with <@&${baseRole}>?`,
+      embeds: [
+        {
+          description: "As you select roles, I'll add them to this list!"
+        }
+      ],
+      components: [
+        {
+          type: 1, 
+          components: [
+            {
+              type: 3,
+              custom_id: "menu-test",
+              options: selectMenuOptions,
+              min_values: 1,
+              max_values: 25
+            }
+          ]
+        },
+        {
+          type: 1,
+          components: [
+            {
+              type: 2,
+              label: "Previous page",
+              style: 2,
+              custom_id: "previous",
+              disabled: true
+            },
+            {
+              type: 2,
+              label: "Page 1",
+              style: 2,
+              custom_id: "submit",
+              disabled: true
+            },
+            {
+              type: 2,
+              label: "Next page",
+              style: 2,
+              custom_id: "next",
+              disabled: true
+            }
+          ]
+        },
+        {
+          type: 1,
+          components: [
+            {
+              type: 2,
+              label: "Create role group",
+              style: 2,
+              custom_id: "submit"
+            }
+          ]
+        }
+      ]
+    });
 
   }
 });
